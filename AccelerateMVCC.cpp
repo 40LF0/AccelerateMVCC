2c6c6340838fcae1ed9313123e3b9f78037ef99b
Seung Yeon Chung <seungyeon.chung@partridgesystems.com>
2023-06-18 16:30:06 +0900

    Add project files.

---
+++ AccelerateMVCC.cpp
@@ -0,0 +1,12 @@
+﻿// AccelerateMVCC.cpp : Defines the entry point for the application.
+//
+
+#include "AccelerateMVCC.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
