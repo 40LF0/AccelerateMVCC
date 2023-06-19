89f7db76f799cd1e0a6194876c5c2425707d8af6
2019082851 <2019082851@hanyang.ac.kr>
2023-06-20 03:59:20 +0900

    [feat] initialize kuku hash/ linking kuku hash - open source

--- main.cpp
+++ main.cpp
@@ -2,13 +2,33 @@
 //
 #include <iostream>
 #include "include/accelerateMVCC.h"
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <kuku/kuku.h>
 
 using namespace std;
+using namespace kuku;
 
 void trxManagerTest();
 
+
+
+
+
 int main()
 {
+
+	auto table_size = static_cast<table_size_type>(5);
+	auto stash_size = static_cast<table_size_type>(5);
+	uint8_t loc_func_count = static_cast<uint8_t>(1);
+	item_type loc_func_seed = make_random_item();
+	uint64_t max_probe = static_cast<uint64_t>(1);
+	item_type empty_item = make_item(0, 0);
+
+	KukuTable table(table_size, stash_size, loc_func_count, loc_func_seed, max_probe, empty_item);
+
+
 	cout << "Hello CMake." << endl;
 
 	trxManagerTest();
