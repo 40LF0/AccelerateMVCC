c86b7c68236e4d90d2d3152c9faf7224a4c69ec6
Seung Yeon Chung <seungyeon.chung@partridgesystems.com>
2023-07-15 19:38:22 +0900

    [feat] handling kuku

--- main.cpp
+++ main.cpp
@@ -14,11 +14,28 @@ void trxManagerTest();
 
 
 
+void print_table(const KukuTable& table)
+{
+	table_size_type col_count = 8;
+	for (table_size_type i = 0; i < table.table_size(); i++)
+	{
+		const auto& item = table.table(i);
+		cout << setw(5)
+			<< i << ": " << setw(5) << get_high_word(item) << "," << get_low_word(item)
+			<< ((i % col_count == col_count - 1) ? "\n" : "\t");
+	}
 
+	cout << endl << endl << "Stash: " << endl;
+	for (table_size_type i = 0; i < table.stash().size(); i++)
+	{
+		const auto& item = table.stash(i);
+		cout << i << ": " << get_high_word(item) << "," << get_low_word(item) << endl;
+	}
+	cout << endl;
+}
 
 int main()
 {
-
 	auto table_size = static_cast<table_size_type>(5);
 	auto stash_size = static_cast<table_size_type>(5);
 	uint8_t loc_func_count = static_cast<uint8_t>(1);
@@ -28,10 +45,44 @@ int main()
 
 	KukuTable table(table_size, stash_size, loc_func_count, loc_func_seed, max_probe, empty_item);
 
+	item_type item1 = make_item(100, 101);
+
+
+	if (table.insert(item1)) {
+		std::cout << "Item inserted successfully." << std::endl;
+	}
+	else {
+		std::cout << "Failed to insert item." << std::endl;
+	}
+
+
+	QueryResult result = table.query(make_item(0, 101));
+
+	QueryResult res = result;
+	cout << "Found: " << boolalpha << !!res << endl;
+	if (res)
+	{
+		cout << "Location: " << res.location() << endl;
+		cout << "In stash: " << boolalpha << res.in_stash() << endl;
+		cout << "Hash function index: " << res.loc_func_index() << endl << endl;
+	}
+	if (result) {
+		std::cout << "Item found at location: " << result.location() << std::endl;
+	}
+	else {
+		std::cout << "Item not found." << std::endl;
+	}
+	table.all_locations(make_item(100, 1));
+	std::cout << table.all_locations(make_item(100, 1)).size() << std::endl;
+	std::cout << get_low_word(table.table(result.location())) << std::endl;
+	std::cout << get_high_word(table.table(result.location()))  << std::endl;
+	print_table(table);
+
 
 	cout << "Hello CMake." << endl;
 
 	trxManagerTest();
+
 
 
 	return 0;
