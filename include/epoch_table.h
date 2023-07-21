39dd129110f7863f00cbc3370004ab8955c0a3b3
2019082851 <2019082851@hanyang.ac.kr>
2023-07-21 18:07:42 +0900

    [feat] add TODO about gc operation in epoch_table

--- include/epoch_table.h
+++ include/epoch_table.h
@@ -49,7 +49,7 @@ namespace mvcc {
             epoch_table_node *table_node = table.at(index);
             table_node->count.fetch_add(1);
             if (epoch_num < table_node->epoch_num) { // NOLINT(bugprone-branch-clone)
-                // something goes wrong..
+                // GC is already performeds
                 table_node->count.fetch_sub(1);
                 insert_to_dummy(epoch);
                 return false;
