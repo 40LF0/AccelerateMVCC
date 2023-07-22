bda6ed1e7eadd15a18266bb5b3a2680bebd5dd4e
2019082851 <2019082851@hanyang.ac.kr>
2023-07-22 20:23:34 +0900

    [feat] add TODO about gc operation in epoch_table

--- include/epoch_table.h
+++ include/epoch_table.h
@@ -36,7 +36,7 @@ namespace mvcc {
     public:
         epoch_table() {
             for (int i = 0; i < EPOCH_TABLE_SIZE; i++) {
-                table.at(i) = new epoch_table_node(i);
+                table.at(i).store(new epoch_table_node(i));
                 auto* wrapper_dummy = new epoch_node_wrapper(nullptr);
                 first_dummy_node.store(wrapper_dummy);
                 last_dummy_node.store(wrapper_dummy);
@@ -46,7 +46,7 @@ namespace mvcc {
         bool insert(epoch_node *epoch) {
             uint64_t epoch_num = epoch->epoch_num;
             uint64_t index = epoch_num / EPOCH_TABLE_SIZE;
-            epoch_table_node *table_node = table.at(index);
+            epoch_table_node *table_node = table.at(index).load();
             table_node->count.fetch_add(1);
             if (epoch_num < table_node->epoch_num) { // NOLINT(bugprone-branch-clone)
                 // GC is already performed
@@ -74,18 +74,32 @@ namespace mvcc {
             };
         }
 
-        // TODO : when this function is called ??
+        // TODO : when this function is called ?? - every 25(EPOCH_TABLE_SIZE/4) times! 50, 75, 100 …
+        //  -> do not start at 25!
         // we need to separate 2 phase of gc processing
         // Phase1 : send epoch_tables nodes to LLT vector
         // <- (epoch_num - epoch_table_size/4)  ~ (epoch_num)
         // Phase2 : processing gc in LLT vector
         bool garbage_collect(uint64_t epoch_num) {
+            // get index range to move elements from table to long_live_epochs
+            uint64_t start_index = (epoch_num - EPOCH_TABLE_SIZE / 2) / EPOCH_TABLE_SIZE;
+            uint64_t end_index = (epoch_num - EPOCH_TABLE_SIZE / 4 - 1) / EPOCH_TABLE_SIZE;
+            for (uint64_t i = start_index; i <= end_index; i++) {
+                epoch_table_node *prev_table_node = table.at(i).load();
+                long_live_epochs.emplace_back(prev_table_node);
+                auto *new_table_node = new epoch_table_node(prev_table_node->epoch_num + EPOCH_TABLE_SIZE);
+                table.at(i).store(new_table_node);
+            }
+
+            //get size of long_live_epochs and operate gc from (size - EPOCH_TABLE_SIZE / 2) to (size - EPOCH_TABLE_SIZE / 4 - 1)
+
+            //if logic is completed, then change false to true
             return false;
         }
 
 
     private:
-        std::array<epoch_table_node *, EPOCH_TABLE_SIZE> table{};
+        std::array<std::atomic<epoch_table_node *>, EPOCH_TABLE_SIZE> table{};
         std::vector<epoch_table_node *> long_live_epochs;
         std::atomic<epoch_node_wrapper *> first_dummy_node;
         std::atomic<epoch_node_wrapper *> last_dummy_node;
